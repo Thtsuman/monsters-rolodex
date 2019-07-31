5f6416ec24e8f14fa402f2608881350d4c8e0145
thtsuman <tht.suman@gmail.com>
2019-07-31 20:26:34 +0530

    Updates

---
+++ precache-manifest.6fa5aa8ed01c109eb22e2003c2af566e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7631eabe73ffdb4d6750a71b9eeadd",
+    "url": "/monters-rolodex/index.html"
+  },
+  {
+    "revision": "106ace47ce8219200164",
+    "url": "/monters-rolodex/static/css/main.a80555a8.chunk.css"
+  },
+  {
+    "revision": "ab76449773f040809588",
+    "url": "/monters-rolodex/static/js/2.61d2c586.chunk.js"
+  },
+  {
+    "revision": "106ace47ce8219200164",
+    "url": "/monters-rolodex/static/js/main.221993f8.chunk.js"
+  },
+  {
+    "revision": "0dde53a42e383d12dfdf",
+    "url": "/monters-rolodex/static/js/runtime~main.8e24775f.js"
+  }
+]);
